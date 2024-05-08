e409c98adfe6bcdaf28ddcc67de69a2bc2ed6347
Daniel Clarke <u8sand@gmail.com>
2024-05-07 22:54:39 -0400

    Update downloads

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -17,7 +17,7 @@ export default function Download() {
         Gene set libraries are available for download in GMT format. Accompanying metadata is available in JSON format which contains gene set condition titles and GSM ids.
       </p>
       <div className="grid lg:grid-cols-2 gap-4 my-4">
-        <a className="stats shadow" href="https://minio.dev.maayanlab.cloud/rummageo/human_12-04-2023.gmt.gz" download="human-geo-auto.gmt.gz">
+        <a className="stats shadow" href="https://s3.amazonaws.com/maayanlab-public/rummageo/human_12-04-2023.gmt.gz" download="human-geo-auto.gmt.gz">
           <div className="stat gap-2">
             <div className="stat-title">human-geo-auto.gmt.gz</div>
             <div className="stat-value text-xl">135,264 gene sets</div>
@@ -26,7 +26,7 @@ export default function Download() {
             </div>
           </div>
         </a>
-        <a className="stats shadow" href="https://minio.dev.maayanlab.cloud/rummageo/gse_processed_meta_human.json" download="geo_processed_meta_human.json">
+        <a className="stats shadow" href="https://s3.amazonaws.com/maayanlab-public/rummageo/gse_processed_meta_human.json" download="geo_processed_meta_human.json">
           <div className="stat gap-2">
             <div className="stat-title">geo_processed_meta_human.json</div>
             <div className="stat-value text-xl">human metadata</div>
@@ -35,7 +35,7 @@ export default function Download() {
             </div>
           </div>
         </a>
-        <a className="stats shadow" href="https://minio.dev.maayanlab.cloud/rummageo/mouse_12-04-2023.gmt.gz" download="mouse-geo-auto.gmt.gz">
+        <a className="stats shadow" href="https://s3.amazonaws.com/maayanlab-public/rummageo/mouse_12-04-2023.gmt.gz" download="mouse-geo-auto.gmt.gz">
           <div className="stat gap-2">
             <div className="stat-title">mouse-geo-auto.gmt.gz</div>
             <div className="stat-value text-xl">158,062 gene sets</div>
@@ -44,7 +44,7 @@ export default function Download() {
             </div>
           </div>
         </a>
-        <a className="stats shadow" href="https://minio.dev.maayanlab.cloud/rummageo/gse_processed_meta_mouse.json" download="gse_processed_meta_mouse.json">
+        <a className="stats shadow" href="https://s3.amazonaws.com/maayanlab-public/rummageo/gse_processed_meta_mouse.json" download="gse_processed_meta_mouse.json">
           <div className="stat gap-2">
             <div className="stat-title">gse_processed_meta_mouse.json</div>
             <div className="stat-value text-xl">mouse metadata</div>
@@ -53,7 +53,7 @@ export default function Download() {
             </div>
           </div>
         </a>
-        <a className="stats shadow" href="https://s3.dev.maayanlab.cloud/rummageo/random50k_co-occurrence_coding.f" download="human_random50k_co-occurrence_coding.f">
+        <a className="stats shadow" href="https://s3.amazonaws.com/maayanlab-public/rummageo/random50k_co-occurrence_coding.f" download="human_random50k_co-occurrence_coding.f">
           <div className="stat gap-2">
             <div className="stat-title">human_random50k_co-occurrence_coding.f</div>
             <div className="stat-value text-xl">human coding co-occurence</div>
@@ -62,7 +62,7 @@ export default function Download() {
             </div>
           </div>
         </a>
-        <a className="stats shadow" href="https://s3.dev.maayanlab.cloud/rummageo/random50k_co-occurrence_noncoding.f" download="human_random50k_co-occurrence_noncoding.f">
+        <a className="stats shadow" href="https://s3.amazonaws.com/maayanlab-public/rummageo/random50k_co-occurrence_noncoding.f" download="human_random50k_co-occurrence_noncoding.f">
           <div className="stat gap-2">
             <div className="stat-title">human_random50k_co-occurrence_noncoding.f</div>
             <div className="stat-value text-xl">human noncoding co-occurence</div>
@@ -71,7 +71,7 @@ export default function Download() {
             </div>
           </div>
         </a>
-        <a className="stats shadow" href="https://s3.dev.maayanlab.cloud/rummageo/mouse_random50k_co-occurrence_coding.f" download="mouse_random50k_co-occurrence_coding.f">
+        <a className="stats shadow" href="https://s3.amazonaws.com/maayanlab-public/rummageo/mouse_random50k_co-occurrence_coding.f" download="mouse_random50k_co-occurrence_coding.f">
           <div className="stat gap-2">
             <div className="stat-title">mouse_random50k_co-occurrence_coding.f</div>
             <div className="stat-value text-xl">mouse coding co-occurence</div>
@@ -80,7 +80,7 @@ export default function Download() {
             </div>
           </div>
         </a>
-        <a className="stats shadow" href="https://s3.dev.maayanlab.cloud/rummageo/mouse_random50k_co-occurrence_noncoding.f" download="mouse_random50k_co-occurrence_noncoding.f">
+        <a className="stats shadow" href="https://s3.amazonaws.com/maayanlab-public/rummageo/mouse_random50k_co-occurrence_noncoding.f" download="mouse_random50k_co-occurrence_noncoding.f">
           <div className="stat gap-2">
             <div className="stat-title">mouse_random50k_co-occurrence_noncoding.f</div>
             <div className="stat-value  text-xl">mouse noncoding co-occurence</div>
